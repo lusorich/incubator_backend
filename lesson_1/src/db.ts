63364834a02acd5a8771ca84dde9fcd9ae2579f5
dreamonaut <dreamonautre@gmail.com>
2024-01-30 22:47:44 +0500

    Improve db methods

--- lesson_1/src/db.ts
+++ lesson_1/src/db.ts
@@ -14,8 +14,8 @@ export const generateRandomVideo = () => ({
 export class LocalDB {
   private db;
 
-  constructor(db: VIDEO[] = []) {
-    this.db = db;
+  constructor(initialDB: VIDEO[] = []) {
+    this.db = initialDB;
   }
 
   getAllVideos() {
@@ -26,5 +26,27 @@ export class LocalDB {
     this.db.push(video);
 
     return new LocalDB(this.db);
+  }
+
+  deleteVideo(id: VIDEO["id"]) {
+    const updatedDb = this.db.filter(({ id: videoId }) => videoId !== id);
+
+    return new LocalDB(updatedDb);
+  }
+
+  getVideoById(id: VIDEO["id"]) {
+    return this._findVideoByKeyValue("id", id);
+  }
+
+  updateVideoById(id: VIDEO["id"], updatedFields: VIDEO) {
+    const found = this._findVideoByKeyValue("id", id);
+
+    const updatedVideo = { ...found, ...updatedFields };
+
+    //TODO: Сгенерировать новый массив с обновленным видео
+  }
+
+  private _findVideoByKeyValue<K extends keyof VIDEO>(key: K, value: VIDEO[K]) {
+    return this.db.find((video) => video[key] === value);
   }
 }
