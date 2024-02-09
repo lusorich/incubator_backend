6ca90cde2ff283ba6a3e4adf48f4d90e5da10803
dreamonaut <dreamonautre@gmail.com>
2024-02-09 19:28:59 +0500

    lesson_2: update auth

--- lesson_2/src/app.ts
+++ lesson_2/src/app.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import routes from "./routes/routes";
-import { SETTINGS } from "./constants";
+import { HTTP_STATUS, SETTINGS } from "./constants";
+import { type Response, type Request, NextFunction } from "express";
 
 export const app = express();
 
 app.use(express.json());
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  if (
+    !req.originalUrl.includes("testing") &&
+    req.method !== "GET" &&
+    (!req.headers.authorization ||
+      req.headers.authorization !== "YWRtaW4gcXdlcnR5")
+  ) {
+    return res.sendStatus(HTTP_STATUS.NO_AUTH);
+  }
+
+  return next();
+});
 
 routes(app);
 
