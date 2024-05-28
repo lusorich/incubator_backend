198c537998a3cdb0611e83a71cf5bbcb494d542d
dreamonaut <dreamonautre@gmail.com>
2024-05-28 23:34:34 +0500

    lesson_12: done

---
+++ lesson_12/src/common/middlewares/auth.middleware.ts
@@ -0,0 +1,110 @@
+import { NextFunction, Request, Response } from 'express';
+import { HTTP_STATUS } from '../../constants';
+import { jwtService } from '../services/jwt.service';
+import { usersQueryRepository } from '../../features/users/repositories/users.query.repository';
+import { requestsService } from '../../features/requests/application/requests.service';
+import { requestsQueryRepository } from '../../features/requests/repositories/requests.query.repository';
+import { formatISO, parseISO } from 'date-fns';
+import { sessionQueryRepository } from '../../repositories/query/sessions.query.repository';
+import { COMMON_RESULT_STATUSES } from '../types/common.types';
+
+export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
+  if (
+    !req.headers.authorization ||
+    req.headers.authorization !== 'Basic YWRtaW46cXdlcnR5'
+  ) {
+    return res.sendStatus(HTTP_STATUS.NO_AUTH);
+  }
+
+  return next();
+};
+
+export const checkJwtAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (!req.headers.authorization) {
+    return res.sendStatus(HTTP_STATUS.NO_AUTH);
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+
+  const id = jwtService.getIdFromToken(token);
+  const isValid = jwtService.isValid(token);
+
+  if (!isValid) {
+    return res.sendStatus(HTTP_STATUS.NO_AUTH);
+  }
+
+  if (id) {
+    try {
+      const user = await usersQueryRepository.getUserById(id);
+
+      if (!user || !user.id) {
+        return res.sendStatus(HTTP_STATUS.NO_AUTH);
+      }
+
+      req.userId = user.id;
+    } catch (e) {
+      return res.sendStatus(HTTP_STATUS.NO_AUTH);
+    }
+  } else {
+    return res.sendStatus(HTTP_STATUS.NO_AUTH);
+  }
+
+  return next();
+};
+
+export const checkRefreshToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const refreshToken = req.cookies?.refreshToken;
+
+  if (!refreshToken) {
+    return res.sendStatus(HTTP_STATUS.NO_AUTH);
+  }
+
+  const isValid = jwtService.isValid(refreshToken);
+
+  if (!isValid) {
+    return res.sendStatus(HTTP_STATUS.NO_AUTH);
+  }
+
+  const userId = jwtService.getIdFromToken(refreshToken);
+  const deviceIdFromToken = jwtService.getDeviceIdFromToken(refreshToken);
+  const iatFromToken = jwtService.getIatFromToken(refreshToken);
+
+  const currentSession = await sessionQueryRepository.getSession(
+    userId,
+    deviceIdFromToken,
+    iatFromToken || '',
+  );
+
+  if (currentSession.status === COMMON_RESULT_STATUSES.NOT_FOUND) {
+    return res.sendStatus(HTTP_STATUS.NO_AUTH);
+  }
+
+  return next();
+};
+export const checkRequestCount = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const url = req.originalUrl;
+  const ip = req.ip || '';
+  const date = parseISO(new Date().toISOString());
+
+  const rateRequests = await requestsQueryRepository.getRateRequests(
+    ip,
+    url,
+    date,
+  );
+
+  await requestsService.addRequest(ip, url, date);
+
+  return next();
+};
