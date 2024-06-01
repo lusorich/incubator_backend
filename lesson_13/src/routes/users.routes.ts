9f401d164b3603e094225ce33850357753e3effd
dreamonaut <dreamonautre@gmail.com>
2024-06-01 20:21:14 +0500

    lesson_13: init

---
+++ lesson_13/src/routes/users.routes.ts
@@ -0,0 +1,61 @@
+import { Router, type Response, type Request } from 'express';
+import { ENDPOINTS, HTTP_STATUS } from '../constants';
+import { getFiltersFromQuery, getFormattedErrors } from '../helpers';
+import { usersQueryRepository } from '../features/users/repositories/users.query.repository';
+import { checkSchema, validationResult } from 'express-validator';
+import { usersSchema } from '../schemas/users.schema';
+import { usersService } from '../features/users/application/users.service';
+
+export const usersRouter = Router({});
+
+usersRouter
+  .route(ENDPOINTS.USERS)
+  .get(async (req: Request, res: Response) => {
+    const {
+      sortBy,
+      sortDirection,
+      pagination,
+      searchEmailTerm,
+      searchLoginTerm,
+    } = getFiltersFromQuery(req.query);
+
+    const users = await usersQueryRepository.getAllUsers({
+      pagination,
+      sortDirection,
+      sortBy,
+      searchEmailTerm,
+      searchLoginTerm,
+    });
+
+    res.status(HTTP_STATUS.SUCCESS).json(users);
+  })
+  .post(
+    checkSchema(usersSchema, ['body']),
+    async (req: Request, res: Response) => {
+      const errors = validationResult(req).array({ onlyFirstError: true });
+
+      if (errors.length) {
+        const formattedErrors = getFormattedErrors(errors);
+
+        return res.status(HTTP_STATUS.INCORRECT).json(formattedErrors);
+      }
+
+      const newUser = await usersService.addUser(req.body);
+
+      return res.status(HTTP_STATUS.CREATED).json(newUser || undefined);
+    },
+  );
+
+usersRouter
+  .route(ENDPOINTS.USERS_ID)
+  .delete(async (req: Request, res: Response) => {
+    const found = await usersQueryRepository.getUserById(req.params.id);
+
+    if (!found) {
+      return res.sendStatus(HTTP_STATUS.NOT_FOUND);
+    }
+
+    await usersService.deleteUserById(req.params.id);
+
+    return res.sendStatus(HTTP_STATUS.NO_CONTENT);
+  });
