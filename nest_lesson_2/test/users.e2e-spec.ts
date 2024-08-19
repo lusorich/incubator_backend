7ba0b14f8be6aa21931905166266fad77ce2653d
dreamonaut <dreamonautre@gmail.com>
2024-08-19 21:53:46 +0500

    nest

--- nest_lesson_2/test/users.e2e-spec.ts
+++ nest_lesson_2/test/users.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { testInitSettings } from 'src/common/test-init-settings';
+import { faker } from '@faker-js/faker';
 
 describe('Users tests', () => {
   let app: INestApplication;
@@ -20,7 +21,57 @@ describe('Users tests', () => {
   test('user should created', () => {
     return request(httpServer)
       .post('/users')
-      .send({ login: 'fdsfds', email: 'fdsfds@mail.ru', password: 'fdfdRR' })
+      .send({
+        login: faker.person.firstName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 8 }),
+      })
       .expect(HttpStatus.CREATED);
+  });
+
+  test('should return correct response body', async () => {
+    const login = faker.person.firstName();
+    const email = faker.internet.email();
+    const password = faker.internet.password({ length: 8 });
+
+    const response = await request(httpServer).post('/users').send({
+      login,
+      email,
+      password,
+    });
+
+    expect(response.body).toMatchObject({ login, email });
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('createdAt');
+  });
+
+  test('should failed if login empty', () => {
+    return request(httpServer)
+      .post('/users')
+      .send({
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 8 }),
+      })
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  test('should failed if email empty', () => {
+    return request(httpServer)
+      .post('/users')
+      .send({
+        login: faker.person.firstName(),
+        password: faker.internet.password({ length: 8 }),
+      })
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  test('should failed if password empty', () => {
+    return request(httpServer)
+      .post('/users')
+      .send({
+        login: faker.person.firstName(),
+        email: faker.internet.email(),
+      })
+      .expect(HttpStatus.BAD_REQUEST);
   });
 });
