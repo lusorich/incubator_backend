8065f7a8e78efb4f5b4150898f60faa8edf62fba
dreamonaut <dreamonautre@gmail.com>
2024-11-18 21:18:19 +0100

    nest 3

---
+++ nest_lesson_3/src/features/posts/models/posts.output.model.ts
@@ -0,0 +1,23 @@
+import { PostDocument } from '../domain/post.entity';
+
+export const postOutputModelMapper = (post?: PostDocument | null) => {
+  if (!post) {
+    return null;
+  }
+  return {
+    id: post.id,
+    title: post.title,
+    shortDescription: post.shortDescription,
+    content: post.content,
+    blogId: post.blogId,
+    blogName: post.blogName,
+    createdAt: post.createdAt,
+    extendedLikesInfo: {
+      likesCount: 0,
+      dislikesCount: 0,
+      myStatus: 'None',
+      newestLikes: [],
+      ...(post?.extendedLikesInfo || {}),
+    },
+  };
+};
