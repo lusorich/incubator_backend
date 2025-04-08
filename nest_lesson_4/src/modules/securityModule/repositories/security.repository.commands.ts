b06be3f5a80464197a9fa76371210655c29ae2f4
dreamonaut <dreamonautre@gmail.com>
2025-04-08 19:56:41 +0200

    lesson 4

---
+++ nest_lesson_4/src/modules/securityModule/repositories/security.repository.commands.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import {
+  Security,
+  SecurityDocument,
+  SecurityModelType,
+} from '../domain/security.entity';
+import { CreateDeviceSessionInput } from '../models/security.model';
+
+@Injectable()
+export class SecurityCommandsRepository {
+  constructor(
+    @InjectModel(Security.name) private SecurityModel: SecurityModelType,
+  ) {}
+
+  async createDeviceSession(
+    createDeviceSessionInput: CreateDeviceSessionInput,
+  ) {
+    const deviceSession: SecurityDocument =
+      this.SecurityModel.createDeviceSession(createDeviceSessionInput);
+
+    return this.save(deviceSession);
+  }
+
+  async save(deviceSession: SecurityDocument) {
+    return deviceSession.save();
+  }
+}
--- nest_lesson_4/src/modules/securityModule/security.module.ts
+++ nest_lesson_4/src/modules/securityModule/security.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controller/security.controller';
+import { SecurityCommandsRepository } from './repositories/security.repository.commands';
 
 @Module({
   imports: [],
   controllers: [SecurityController],
-  providers: [],
-  exports: [],
+  providers: [SecurityCommandsRepository],
+  exports: [SecurityCommandsRepository],
 })
 export class SecurityModule {}
